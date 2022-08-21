190c842feca45e8d50c3e96a4e22cdd004a8f222
Aniclet <111573911+Aniclet@users.noreply.github.com>
2022-08-21 23:57:53 +0530

    Add files via upload

---
+++ Coursera Week 4 project.py
@@ -0,0 +1,31 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# # My Jupyter Notebook on IBM Watson Studio
+
+# **Aniclet Noronha**
+# ----
+# Occupation: Project Manager
+
+# *I am interested in data science because of its scope and influence in any given industry*.
+# *Data science is a passion for me considering my logical thinking and curious nature*
+
+# ### The code below is going to help calculate the multiplication of two given numbers
+
+# In[1]:
+
+
+23646666313 * 321364135135
+
+
+# [FACEBOOK](https://www.facebook.com)
+# 
+# ----
+# 
+# 1. Learn Management skills.
+# 2. Earn a Management degree.
+# 3. Get employeed at a good firm.
+# 4. Work hard and move up the ladder.
+# ----
+# IBM logo: 
+# ![alt text](https://www.ibm.com/brand/experience-guides/developer/b1db1ae501d522a1a4b49613fe07c9f1/01_8-bar-positive.svg)
